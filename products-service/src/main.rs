f31bfccc1ad9a54289a096683685635df862b4de
dili91 <andrea.dilisio91@gmail.com>
2022-07-01 17:52:59 +0200

    changes

--- products-service/src/main.rs
+++ products-service/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn up() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/up", web::get().to(up)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
