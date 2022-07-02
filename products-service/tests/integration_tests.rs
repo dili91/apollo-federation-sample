5ba5ecc72dfe39b8dd5ca40ee7eea33dfe2ceecb
dili91 <andrea.dilisio91@gmail.com>
2022-07-02 15:22:05 +0200

    added tests for catalog handler

--- products-service/tests/integration_tests.rs
+++ products-service/tests/integration_tests.rs
@@ -1,7 +1,7 @@
 use std::net::TcpListener;
 
 #[actix_rt::test]
-async fn products_service_is_up() {
+async fn should_be_up() {
     // Arrange
     let address = spawn_app();
     let client = reqwest::Client::new();
@@ -19,6 +19,43 @@ async fn products_service_is_up() {
         "I'm Alive!",
         response.text().await.expect("Failed to get response body")
     );
+}
+
+#[actix_rt::test]
+async fn should_return_a_list_of_products(){
+    // Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get(&format!("{}/products", &address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    //todo: body assertions
+}
+
+#[actix_rt::test]
+async fn should_return_a_product_by_id(){
+// Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let product_id = "123";
+
+    // Act
+    let response = client
+        .get(&format!("{}/products/{}", &address, product_id))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    //todo: body assertions
 }
 
 fn spawn_app() -> String {
